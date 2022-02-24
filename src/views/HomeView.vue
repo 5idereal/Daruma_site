bc214e7e043142e25748401180a372e3f7108ded
SiderealArt <30827929+SiderealArt@users.noreply.github.com>
2022-02-24 21:36:52 +0800

    footer and header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <p class="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">Faster,<br> Stronger,<br>  Better.</p>
 </template>
+
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
