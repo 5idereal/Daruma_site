a01f3c14db068db2a361ccfce5a83b3108cb72b5
SiderealArt <nelson22768384@gmail.com>
2022-02-25 13:10:30 +0800

    navbar as component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+/*import cn from './cn.json'
+import en from './en.json'
+import tw from './tw.json'
+import es from './es.json'
+*/const messages = {
+    //cn,en,tw,es
+}
+const i18n = createI18n({
+
+    locale: 'en', // set locale
+    fallbackLocale: 'es', // set fallback locale
+    messages, // set locale messages
+
+})
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
